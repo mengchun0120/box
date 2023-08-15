456b02e743ffd00db2fd6b93f96c11d70c09ca19
Chun Meng <mengchun0120@gmail.com>
2023-08-15 09:36:39 -0400

    Updated

--- app/src/main/java/com/mcdane/box/GameRenderer.kt
+++ app/src/main/java/com/mcdane/box/GameRenderer.kt
@@ -10,10 +10,13 @@ import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 import kotlin.random.Random
 import android.opengl.GLES30 as GL
+import kotlinx.serialization.*
 
 class GameRenderer(private val context: Context): GLSurfaceView.Renderer {
     private lateinit var program: SimpleProgram
     private lateinit var textSys: TextSystem
+    private val viewportSize = Vector(2)
+    private val viewportOrigin = Vector(2)
     private val board = Board()
     private val boardLeft = 20.0f
     private lateinit var curBox: Box
@@ -26,47 +29,18 @@ class GameRenderer(private val context: Context): GLSurfaceView.Renderer {
     )
 
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
-        Box.init(context.assets)
-
-        GL.glClearColor(1.0f, 1.0f, 1.0f, 1.0f)
-        GL.glEnable(GL.GL_BLEND)
-        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
-
-        program = SimpleProgram(context.resources)
-        program.use()
-
-        textSys = TextSystem(context.assets, "font")
-
-        curBox = Box(0, 0)
+        initOpenGL()
+        initGame()
     }
 
     override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
-        GL.glViewport(0, 0, width, height)
-
-        val viewportSize = Vector(width.toFloat(), height.toFloat())
-        val viewportOrigin = viewportSize / 2.0f
-
-        program.setViewportSize(viewportSize.data)
-        program.setViewportOrigin(viewportOrigin.data)
-
-        board.pos = Vector(
-            boardLeft,
-            (height.toFloat() - board.height) / 2.0f
-        )
-
-        curBoxPos = Vector(
-            board.pos[0] + 1.0f + curBoxCol * Box.BOX_SPAN,
-            board.pos[1] + 1.0f + curBoxRow * Box.BOX_SPAN
-        )
+        resetViewport(width, height)
+        resetGamePos()
     }
 
     override fun onDrawFrame(p0: GL10?) {
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-
-        program.use()
-        program.setAlpha(1.0f)
-        //board.draw(program)
-
+        board.draw(program)
         curBox.draw(program, curBoxPos, colors[0])
     }
 
@@ -82,5 +56,46 @@ class GameRenderer(private val context: Context): GLSurfaceView.Renderer {
 
     fun close() {
         program.close()
+    }
+
+    private fun initOpenGL() {
+        GL.glClearColor(1.0f, 1.0f, 1.0f, 1.0f)
+        GL.glEnable(GL.GL_BLEND)
+        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
+
+        program = SimpleProgram(context.resources)
+        program.use()
+        program.setAlpha(1.0f)
+
+        textSys = TextSystem(context.assets, "font")
+    }
+
+    private fun initGame() {
+        Box.init(context.assets)
+        curBox = Box(0, 0)
+    }
+
+    private fun resetViewport(width: Int, height: Int) {
+        GL.glViewport(0, 0, width, height)
+
+        viewportSize[0] = width.toFloat()
+        viewportSize[1] = height.toFloat()
+        viewportOrigin[0] = viewportSize[0] / 2.0f
+        viewportOrigin[1] = viewportSize[1] / 2.0f
+
+        program.setViewportSize(viewportSize.data)
+        program.setViewportOrigin(viewportOrigin.data)
+    }
+
+    private fun resetGamePos() {
+        board.pos = Vector(
+            boardLeft,
+            (viewportSize[1] - board.height) / 2.0f
+        )
+
+        curBoxPos = Vector(
+            board.pos[0] + 1.0f + curBoxCol * Box.BOX_SPAN,
+            board.pos[1] + 1.0f + curBoxRow * Box.BOX_SPAN
+        )
     }
 }
